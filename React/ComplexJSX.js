8035aabb3a54f2306936a520f3269f0fe95d450a
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-01-24 23:31:45 +0530

    Create ComplexJSX.js

---
+++ React/ComplexJSX.js
@@ -0,0 +1,9 @@
+const JSX = <div>
+      <h1>Heading</h1>
+      <p>Sample Paragraph</p>
+      <ul>
+      <li> List 1 </li>
+      <li> List 2 </li>
+      <li> List 3 </li>
+      </ul>
+      </div>
