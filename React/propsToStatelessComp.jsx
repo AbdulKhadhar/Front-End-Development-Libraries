fe05137cccce58482faa0ce88eb8617ffc4d8d1d
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-01-27 18:18:31 +0530

    Create propsToStatelessComp.jsx

---
+++ React/propsToStatelessComp.jsx
@@ -0,0 +1,27 @@
+const CurrentDate = (props) => {
+  return (
+    <div>
+      { /* Pass Props to a Stateless Functional Component */ }
+      <p>The current date is: {props.date} </p>
+  
+    </div>
+  );
+};
+
+class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>What date is it?</h3>
+
+       
+        <CurrentDate date={Date()} /> 
+       
+
+      </div>
+    );
+  }
+};
