8b09c941640f97c1bc7bea48e4cb35a30f04c64c
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-01-27 17:14:02 +0530

    Create composeComponents.jsx

---
+++ React/composeComponents.jsx
@@ -0,0 +1,33 @@
+class Fruits extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h2>Fruits:</h2>
+
+        <NonCitrus /> // not defined in this page
+        <Citrus /> // not defined in this page
+
+      </div>
+    );
+  }
+};
+
+class TypesOfFood extends React.Component {
+  constructor(props) {
+     super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Types of Food:</h1>
+        
+        <Fruits />
+
+        <Vegetables />
+      </div>
+    );
+  }
+};
