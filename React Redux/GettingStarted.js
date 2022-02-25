4446687b4677414802bcb99c75fcf91d2ec65d36
Abdul Khadar <42581864+AbdulKhadhar@users.noreply.github.com>
2022-02-25 22:52:15 +0530

    Update GettingStarted.js

--- React Redux/GettingStarted.js
+++ React Redux/GettingStarted.js
@@ -1,14 +1,44 @@
 class DisplayMessages extends React.Component {
-  // Change code below this line
-  constructor(props){
+  constructor(props) {
     super(props);
-  this.state={
-    input : "" ,
-    messages : []
+    this.state = {
+      input: '',
+      messages: []
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submitMessage = this.submitMessage.bind(this);
   }
-  }
-  // Change code above this line
+  // Add handleChange() and submitMessage() methods here
+  
+handleChange(event){
+  this.setState({
+    input : event.target.value,
+    messeges : this.state.messeges
+  })
+};
+
+submitMessage(){
+this.setState({
+  input : '',
+  messeges : [...this.state.messeges,this.state.input]
+})
+}
+
   render() {
-    return <div />
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        { /* Render an input, button, and ul below this line */ }
+        <input value={this.state.input} onChange = {this.handleChange} />
+        <button onClick = {this.submitMessage}>Submit</button>
+        <ul/>
+        <ul>
+        {this.state.messages.map((x,i)=>{
+          return <li key={i}>{x}</li>
+        })}
+        </ul>
+        { /* Change code above this line */ }
+      </div>
+    );
   }
 };
